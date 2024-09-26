e81834451ace6fe0a3763e4afd7a2952d01ecbbb
PaulMares <fpmaresquiersanchez@gmail.com>
2024-09-26 14:08:19 -0700

    Moved and added GCD test cases

--- tests/fractions_utilities_tests.py
+++ tests/fractions_utilities_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from fractions import least_common_multiplier, float_to_fraction, Fraction
+from fractions import least_common_multiplier, float_to_fraction, Fraction, greatest_common_divisor
 
 
 class LCMTestCases(unittest.TestCase):
@@ -15,6 +15,19 @@ class LCMTestCases(unittest.TestCase):
         """Tests the LCM of two numbers whose LCM is not equal to their multiplication."""
         self.assertEqual(12, least_common_multiplier(4, 6))
 
+class GCDTestCases(unittest.TestCase):
+    def test_GCD_equal(self):
+        self.assertEqual(10, greatest_common_divisor(10, 10))
+
+    def test_GCD_no_common_divisors(self):
+        self.assertEqual(1, greatest_common_divisor(20, 13))
+
+    def test_GCD_arg_is_GCD(self):
+        self.assertEqual(5, greatest_common_divisor(5, 10))
+
+    def test_GCD_common_divisors(self):
+        self.assertEqual(21, greatest_common_divisor(1071, 462))
+
 class FloatToFractionTestCases(unittest.TestCase):
     def test_int(self):
         """Tests that an integer converts to the equivalent fraction."""
@@ -27,7 +40,6 @@ class FloatToFractionTestCases(unittest.TestCase):
     def test_rational_negative(self):
         """Tests that a rational negative float converts to the equivalent fraction."""
         self.assertEqual(Fraction(-1,2), float_to_fraction(-0.5))
-
 
 
 if __name__ == '__main__':
